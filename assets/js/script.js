82f4da7a57ccf9348919d61d7afd83542fa28d40
ebtaterbug <ityjoseph@gmail.com>
2021-11-16 13:41:07 -0800

    add basic map functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,52 @@
+// Load Map & Markers
+
+var mymap = L.map('map').setView([39.833338, -98.890899], 4);
 
 
-function getCampsites() {
-    var apiUrl = "https://developer.nps.gov/api/v1/campgrounds?stateCode=CA&limit=50&start=0&api_key=UvgAMzaiAVvGaHIArhZU2CAxBdpxtaKQBrhjcWMO";
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+
+// Get Campsite Coords/name/Url
+
+function getCampsites(state) {
+    var apiUrl = "https://developer.nps.gov/api/v1/campgrounds?stateCode=" + state + "&limit=50&start=0&api_key=UvgAMzaiAVvGaHIArhZU2CAxBdpxtaKQBrhjcWMO";
     fetch(apiUrl)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            console.log(data.data[0].latitude + "  " + data.data[0].longitude);
-    });
+            for (var i=0; i < data.data.length; i++) {
+            getCampsiteWeather(data.data[i].latitude, data.data[i].longitude, data.data[i].name, data.data[i].url);
+            }
+        });
 };
 
-getCampsites();
+
+// Search By State
 
 $("#search-btn").on("click", function() {
-    navigator.geolocation.getCurrentPosition(success, error, options);
-
-    var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    };
-
-    function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-
-
-    function success(pos) {
-        var crd = pos.coords;
-        var mymap = L.map('map').setView([crd.latitude, crd.longitude], 9);
-
-        L.marker([36.4646681174428, -118.66967218847]).addTo(mymap);
-        L.marker([34.0143426535482, -119.367763068282]).addTo(mymap);
-
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1
-        }).addTo(mymap);
-    }
+    var state = $("#search-bar").val();
+    getCampsites(state);
 });
 
-$("#map").on("click", function() { 
-    $("#card1").removeAttr("style");
-});
+
+// Get Weather With Campsite Coords & Display
+
+function getCampsiteWeather(lat, lon, campsiteName, campsiteUrl) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude={part}&appid=b451e261be7a552705f2a22389c1a371";
+    fetch(apiUrl)
+      .then(function(response) {
+        return response.json();
+        })
+    .then(function(data) {
+        var marker = L.marker([lat, lon]).addTo(mymap);
+        marker.bindPopup("<b>" + campsiteName + "</b><br><a href=" + campsiteUrl + "  target='_blank'>" + campsiteUrl + "</a><br><b> Current Weather: " + data.current.weather[0].description + ", " + (Math.round(((((data.current.temp)-273.15)*1.8)+32))) + " °F</b>" ).openPopup();
+        console.log(data);
+    });
+};
